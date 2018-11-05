2206558fc896a4b905625665b0ab3520e92d3b51
myu21 <myu21@choate.edu>
2018-11-05 15:18:13 -0500

    deeply flawed

--- maze.py
+++ maze.py
@@ -1,23 +1,48 @@
+from PIL import Image
 import random as r
-width = 50#int(sys.argv[1])
-height = 50#int(sys.argv[2])
+width = 25#int(sys.argv[1])
+height = 25#int(sys.argv[2])
+
+image = Image.new("RGB", (width, height))
 
 board = [[0]*(width) for x in range(height)]
 
-board[r.randint(height)][0] = 1
+def boardcheck(x,y):
+	while True:
+		if x >= 1 and x <= width-1 and y >= 1 and y <= height-1:
+			choose = r.randint(0,3)
+			if choose == 0:
+				if board[y-1][x] == 0:
+					board[y-1][x] = 1
+					boardcheck(y-1,x)
+					break
+			elif choose == 1:
+				if board[y+1][x] ==0:
+					board[y+1][x] = 1
+					boardcheck(y+1,x)
+					break
+			elif choose == 2:
+				if board[y][x-1] ==0:
+					board[y][x-1] = 1
+					boardcheck(y,x-1)
+					break
+			elif choose == 3:
+				if board[y][x+1] ==0:
+					board[y][x+1] = 1
+					boardcheck(y,x+1)
+					break		
+		else:
+			print("working")
+			break	
 
-for x in range(width):
-	for y in range(height):
-		if board[y][x] == 1 and (board[y-1][x]+board[y+1][x]+board[y][x-1]+board[y][x+] == 1):
-			"""
-			while True:
-				choose = r.randint(3)
-				if choose == 0:	
-					board[y][x]
-				elif choose == 1:
+board[1][2] = 1
+board[1][1] = 1
+for x in range(width-1):
+	for y in range(height-1):
+		if board[y][x] == 1 and (board[y-1][x]+board[y+1][x]+board[y][x-1]+board[y][x+1] == 1):
+			boardcheck(x,y)
+			if board[y][x] == 1:
+				image.putpixel((x,y),(255,255,255))
+print(*board)
 
-				elif choose == 2:
-
-				elif choose == 3:
-"""
-def boardcheck(self, x, y):
+image.save("maze.png","PNG")
