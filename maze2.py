5475e7a5756dcf32530622257834aedd7092dfae
myu21 <myu21@choate.edu>
2018-11-09 11:45:11 -0500

    testy test (probably going to try a differen approach if this doesn't work out)

--- maze2.py
+++ maze2.py
@@ -1,13 +1,16 @@
+from PIL import Image
 import random as r
-width = 10
-height = 10
+width = 20
+height = 20
 
 board = [[0]*(width) for x in range(height)]
 
-board[5][5] = 1
+board[7][7] = 1
+board[13][13]=1
+
 
 def boardcheck(x,y):
-	if x <= 0 or x >= width or y <= 0 or y >= height:
+	if x <= 0 or x >= width-1 or y <= 0 or y >= height-1:
 		return "working"
 	deadend = [0,0,0,0]
 	try:
@@ -15,33 +18,33 @@ def boardcheck(x,y):
 			if deadend[0] + deadend[1] + deadend[2] + deadend[3] <=3:
 				change = r.randint(0,3)
 				if change == 0:
-					if board[y-1][x] != 1 and board[y-1][x-1] != 1 and board[x+1][y-1] !=1:
+					if board[y-1][x] != 1 and board[y-1][x-1] != 1 and board[y-1][x+1] !=1 and board[y-2][x] != 1:
 						board[y-1][x] = 1
-						boardcheck(y-1,x)
+						boardcheck(x,y-1)
 						break
 					else:
 						deadend[0] = 1
 						break
 				elif change == 1:
-					if board[y+1][x] != 1 and board[y+1][x-1] != 1 and board[x+1][y+1] !=1:
+					if board[y+1][x] != 1 and board[y+1][x-1] != 1 and board[y+1][x+1] !=1 and board[y+2][x] != 1:
 						board[y+1][x] = 1
-						boardcheck(y+1,x)
+						boardcheck(x,y+1)
 						break
 					else:
 						deadend[1] = 1
 						break
 				elif change == 2:
-					if board[y][x+1] != 1 and board[y-1][x+1] != 1 and board[x+1][y+1] !=1:
+					if board[y][x+1] != 1 and board[y-1][x+1] != 1 and board[y+1][x+1] !=1 and board[y][x+2] != 1:
 						board[y][x+1] = 1
-						boardcheck(y,x+1)
+						boardcheck(x+1,y)
 						break
 					else:
 						deadend[2] = 1
 						break
 				else:
-					if board[y][x-1] != 1 and board[y-1][x-1] != 1 and board[x-1][y+1] !=1:
+					if board[y][x-1] != 1 and board[y-1][x-1] != 1 and board[y+1][x-1] !=1 and board[y][x-2] != 1:
 						board[y][x-1] = 1
-						boardcheck(y,x-1)
+						boardcheck(x-1,y)
 						break
 					else:
 						deadend[3] = 1
@@ -50,7 +53,13 @@ def boardcheck(x,y):
 				break
 	except IndexError:
 		pass
-for z in range(1000):
+
+for z in range(100000):
+	for x in range (1,width-1):
+		for y in range (1,height-1):
+			if board[y][x] == 1 and board[y-1][x]+board[y+1][x]+board[y][x-1]+board[y][x+1] <=1:
+				boardcheck(x,y)
+for z in range(100000):
 	for x in range (1,width-1):
 		for y in range (1,height-1):
 			if board[y][x] == 1 and board[y-1][x]+board[y+1][x]+board[y][x-1]+board[y][x+1] <=1:
@@ -58,3 +67,4 @@ for z in range(1000):
 
 for x in range(len(board)):
 		print(*board[x])
+
